52df298a73129cba6b5cfe415e87e32849726498
Hayden Randall <haydenjrandall@gmail.com>
2023-07-27 07:21:36 -0700

    refactored code and added some to p2

---
+++ project 2 goals/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoals: ['Finish the course and learn vue!!!','hi mom']
+
+        }
+    }
+});
+app.mount('#user-goal');
