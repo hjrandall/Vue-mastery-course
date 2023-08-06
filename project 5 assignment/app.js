a92b728861826c2ccaa9540745511b6af7705b7f
Hayden Randall <haydenjrandall@gmail.com>
2023-08-05 18:28:46 -0700

    did project 5

--- project 5 assignment/app.js
+++ project 5 assignment/app.js
@@ -1,11 +1,15 @@
 const app = Vue.createApp({
     data(){
         return {
-
+            inputClass:'',
+            paragraphVisy: true,
+            dynamicCss: {inputClass: true, visible: this.paragraphVisy, hidden: this.paragraphVisy}
         }
     },
     methods:{
-
+        visi(){
+            this.paragraphVisy = !this.paragraphVisy;
+        }
     },
     computed:{
 
